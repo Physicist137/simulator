367d9eba224f788fd9a5045b284c049b44b3b07b
Physicist137 <physicist093@yahoo.com.br>
2021-08-27 16:30:24 -0300

    Fix Simple Polygon Tests

--- test/gtest_src/math/geometry/2D/simple_polygon.cpp
+++ test/gtest_src/math/geometry/2D/simple_polygon.cpp
@@ -4,12 +4,6 @@
 #include <math/geometry/2D/simple_polygon.hpp>
 
 
-bool almost_equal(float value, float expected, float error = 1e-6) {
-	if (value - expected > error) return false;
-	if (value - expected < -error) return false;
-	return true;
-}
-
 TEST(SimplePolygon2D, ConstructorAndBasicFunctions) {
 	// Define few vectors.
 	math::linear::StaticVector<float, 2> zero({0.0, 0.0});
@@ -29,16 +23,16 @@ TEST(SimplePolygon2D, ConstructorAndBasicFunctions) {
 	triangle.addVertex(j);
 	triangle.addVertex(i);
 	
-	math::geometry2::SimplePolygon<float> trapezium;
-	trapezium.addVertex(zero);
-	trapezium.addVertex(j);
-	trapezium.addVertex(i+j);
-	trapezium.addVertex(2.0f * i);
+	math::geometry2::SimplePolygon<float> trapezoid;
+	trapezoid.addVertex(zero);
+	trapezoid.addVertex(j);
+	trapezoid.addVertex(i+j);
+	trapezoid.addVertex(2.0f * i);
 
 	EXPECT_FLOAT_EQ(square.perimeter(), 4.0);
 	EXPECT_FLOAT_EQ(square.area(), 1.0);
 	EXPECT_FLOAT_EQ(triangle.perimeter(), 2.0 + std::sqrt(2.0));
 	EXPECT_FLOAT_EQ(triangle.area(), 0.5);
-	EXPECT_FLOAT_EQ(trapezium.perimeter(), 4.0 + std::sqrt(2.0));
-	EXPECT_FLOAT_EQ(trapezium.area(), 3.0 / 2.0);
+	EXPECT_FLOAT_EQ(trapezoid.perimeter(), 4.0 + std::sqrt(2.0));
+	EXPECT_FLOAT_EQ(trapezoid.area(), 3.0 / 2.0);
 }
