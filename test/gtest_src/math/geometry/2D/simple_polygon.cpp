28499677c93b1e93f0994b69a4cce626c3c11dd4
Physicist137 <physicist093@yahoo.com.br>
2021-08-27 16:28:04 -0300

    Add more polygons for testing

--- test/gtest_src/math/geometry/2D/simple_polygon.cpp
+++ test/gtest_src/math/geometry/2D/simple_polygon.cpp
@@ -1,7 +1,14 @@
 #include <gtest/gtest.h>
+#include <cmath>
 #include <math/linear/static_vector.hpp>
 #include <math/geometry/2D/simple_polygon.hpp>
 
+
+bool almost_equal(float value, float expected, float error = 1e-6) {
+	if (value - expected > error) return false;
+	if (value - expected < -error) return false;
+	return true;
+}
 
 TEST(SimplePolygon2D, ConstructorAndBasicFunctions) {
 	// Define few vectors.
@@ -16,7 +23,22 @@ TEST(SimplePolygon2D, ConstructorAndBasicFunctions) {
 	square.addVertex(i);
 	square.addVertex(i+j);
 	square.addVertex(j);
+	
+	math::geometry2::SimplePolygon<float> triangle;
+	triangle.addVertex(zero);
+	triangle.addVertex(j);
+	triangle.addVertex(i);
+	
+	math::geometry2::SimplePolygon<float> trapezium;
+	trapezium.addVertex(zero);
+	trapezium.addVertex(j);
+	trapezium.addVertex(i+j);
+	trapezium.addVertex(2.0f * i);
 
-	EXPECT_EQ(square.perimeter(), 4.0);
-	EXPECT_EQ(square.area(), 1.0);
+	EXPECT_FLOAT_EQ(square.perimeter(), 4.0);
+	EXPECT_FLOAT_EQ(square.area(), 1.0);
+	EXPECT_FLOAT_EQ(triangle.perimeter(), 2.0 + std::sqrt(2.0));
+	EXPECT_FLOAT_EQ(triangle.area(), 0.5);
+	EXPECT_FLOAT_EQ(trapezium.perimeter(), 4.0 + std::sqrt(2.0));
+	EXPECT_FLOAT_EQ(trapezium.area(), 3.0 / 2.0);
 }
