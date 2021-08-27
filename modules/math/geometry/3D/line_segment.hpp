e14250c2db61afda6da5e14cb47c059750c37900
Physicist137 <physicist093@yahoo.com.br>
2021-08-26 22:06:07 -0300

    Create 3D line segment class

---
+++ modules/math/geometry/3D/line_segment.hpp
@@ -0,0 +1,126 @@
+#pragma once
+#include <iostream>
+#include <math/linear/static_vector.hpp>
+#include <math/linear/static_matrix.hpp>
+
+namespace math {
+namespace geometry3 {
+
+template <typename T>
+class LineSegment {
+	math::linear::StaticVector<T, 3> _start;
+	math::linear::StaticVector<T, 3> _end;
+
+protected:
+	class IntersectionData;
+
+public:
+	explicit LineSegment(const math::linear::StaticVector<T, 3>& start, const math::linear::StaticVector<T, 3>& end) : _start(start), _end(end) {}
+	explicit LineSegment(const math::linear::StaticVector<T, 3>& end) : _start(), _end(end) {}
+
+	inline const math::linear::StaticVector<T, 3>& start() const {return _start;}
+	inline const math::linear::StaticVector<T, 3>& end() const {return _end;}
+	
+	math::linear::StaticVector<T, 3> displacement() const;
+	T length_squared() const;
+
+	IntersectionData intersect(const LineSegment& other) const;
+};
+
+
+template <typename T>
+class LineSegment<T>::IntersectionData {
+	bool _intersects;
+	T _this;
+	T _other;
+	math::linear::StaticVector<T, 3> _position;
+
+public:
+	explicit IntersectionData() : _intersects(false), _this(), _other(), _position() {}
+
+	IntersectionData(
+		const T& this_position,
+		const T& other_position,
+		const math::linear::StaticVector<T, 3>& position
+	) : _intersects(true),  _this(this_position), _other(other_position), _position(position) {}
+
+	inline const bool intersects() const {return _intersects;}
+	inline const bool hits() const {return _intersects;}
+	inline const bool doesIntersect() const {return _intersects;}
+	inline const bool hasHit() const {return _intersects;}
+
+	inline const T& thisLine() const {return _this;}
+	inline const T& otherLine() const {return _other;}
+
+	inline const T& t() const {return _this;}
+	inline const T& s() const {return _other;}
+
+	inline const math::linear::StaticVector<T, 3>& position() const {return _position;}
+};
+
+template <typename T>
+inline math::linear::StaticVector<T, 3> LineSegment<T>::displacement() const {
+	return _end - _start;
+}
+
+template <typename T>
+inline T LineSegment<T>::length_squared() const {
+	return this->displacement().dot();
+}
+
+template <typename T>
+typename LineSegment<T>::IntersectionData LineSegment<T>::intersect(const LineSegment& other) const {
+	// Get differences.
+	math::linear::StaticVector<T, 3> diff_this = _end - _start;
+	math::linear::StaticVector<T, 3> diff_other = other._end - other._start;
+	math::linear::StaticVector<T, 3> diff_cross = other._start - _start;
+
+	// Get matrix elements.
+	T a11 = diff_this.x();
+	T a21 = diff_this.y();
+	T a31 = diff_this.z();
+	T a12 = diff_other.x();
+	T a22 = diff_other.y();
+	T a32 = diff_other.z();
+	T b1 = diff_cross.x();
+	T b2 = diff_cross.y();
+	T b3 = diff_cross.z();
+	
+	// Ignore third dimension. Calculate solution.
+	// Compute determinant and rule out parallel lines.
+	T det = a11 * a22  - a12 * a21;
+	if (det == 0.0) return LineSegment<T>::IntersectionData();
+
+	// Compute solution.
+	T t = (a22*b1 - a12*b2) / det;
+	T s = (a21*b1 - a11*b2) / det;
+
+	// Rule out no intersection scenarios.
+	if (t < 0) return LineSegment<T>::IntersectionData();
+	if (s < 0) return LineSegment<T>::IntersectionData();
+	if (t > 1) return LineSegment<T>::IntersectionData();
+	if (s > 1) return LineSegment<T>::IntersectionData();
+	
+	
+	// Proceed and analyze third dimension
+	// Partial solution by echelon form. Compare with new solution.
+	if (a31 != 0) {
+		T num = b3 * a21 / a31 - b2;
+		T den = a32 * a21 / a31 - a22;
+		if (den != 0  and  s != num / den) return LineSegment<T>::IntersectionData();
+	}
+	
+	else if (a32 != 0) {
+		T num = b3 * a22 / a32 - b2;
+		T den = a31 * a22 / a32 - a21;
+		if (den != 0  and  t != num / den) return LineSegment<T>::IntersectionData();
+	}
+	
+	// Return intersection.
+	math::linear::StaticVector<T, 3> pos = _start + diff_this * t;
+	return LineSegment<T>::IntersectionData(t, s, pos);
+}
+
+
+} // namespace geometry3
+} // namespace math
