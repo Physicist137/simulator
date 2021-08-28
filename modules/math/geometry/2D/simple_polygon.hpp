8c575117e6d1da1b14336f4fbd43cb99d6460a1c
Physicist137 <physicist093@yahoo.com.br>
2021-08-27 21:31:14 -0300

    Create and test intersection functions in Simple Polygon

--- modules/math/geometry/2D/simple_polygon.hpp
+++ modules/math/geometry/2D/simple_polygon.hpp
@@ -5,6 +5,7 @@
 #include <initializer_list>
 #include <math/linear/static_vector.hpp>
 #include <math/geometry/intersection.hpp>
+#include <math/geometry/2D/ray.hpp>
 #include <math/geometry/2D/line_segment.hpp>
 
 namespace math {
@@ -26,14 +27,28 @@ public:
 	// Return special vertices.
 	inline const math::linear::StaticVector<T, 2>& start() const {return _vertices.front();}
 	inline const math::linear::StaticVector<T, 2>& end() const {return _vertices.back();}
-	inline const math::linear::StaticVector<T, 2>& vertex(unsigned i) const {return _vertices[i];}
-	inline const math::linear::StaticVector<T, 2>& operator[](unsigned i) const {return _vertices[i % _vertices.size()];}
+	
+	// Return polygon featurs.
+	const math::linear::StaticVector<T, 2>& vertex(unsigned i) const {return _vertices[i % _vertices.size()];}
+	inline const math::linear::StaticVector<T, 2>& operator[](unsigned i) const {return vertex(i);}
+	math::geometry2::LineSegment<T> edge(unsigned i) const;
 	
 	// Polygon information
 	inline unsigned numberOfEdges() const {return _vertices.size();}
+	inline unsigned numberOfVertices() const {return _vertices.size();}
+	
+	// Position information.
+	math::linear::StaticVector<T, 2> center() const;
+	
+	// Geometrical information.
 	T perimeter(const std::function<T(T)>& sqrt = [](auto const& x) {return std::sqrt(x);}) const;
 	T signedArea() const;
 	T area() const;
+	
+	// Intersection operations.
+	math::geometry::IntersectionData<T,2> intersect(const math::geometry2::Ray<T>& ray) const;
+	math::geometry::IntersectionData<T,2> intersect(const math::geometry2::LineSegment<T>& line) const;
+	bool isInside(const math::linear::StaticVector<T, 2>& point) const;
 	
 	// Add vertices.
 	const SimplePolygon& addVertex(const math::linear::StaticVector<T, 2>& vertex);
@@ -56,6 +71,19 @@ template <typename T>
 const SimplePolygon<T>& SimplePolygon<T>::addVertex(const math::linear::StaticVector<T, 2>& vertex) {
 	_vertices.push_back(vertex);
 	return *this;
+}
+
+template <typename T>
+math::geometry2::LineSegment<T> SimplePolygon<T>::edge(unsigned i) const {
+	return math::geometry2::LineSegment<T>(this->vertex(i), this->vertex(i+1));
+}
+
+template <typename T>
+math::linear::StaticVector<T, 2> SimplePolygon<T>::center() const {
+	math::linear::StaticVector<T, 2> result;
+	T num = static_cast<T>(numberOfVertices());
+	for (const auto& vertex : _vertices) result += vertex / num;
+	return result;
 }
 
 template <typename T>
@@ -93,6 +121,50 @@ T SimplePolygon<T>::area() const {
 	T signed_area = signedArea();
 	if (signed_area >= 0) return signed_area;
 	else return -signed_area;
+}
+
+template <typename T>
+math::geometry::IntersectionData<T,2> SimplePolygon<T>::intersect(const math::geometry2::Ray<T>& ray) const {
+	unsigned size = _vertices.size();
+	math::geometry::IntersectionData<T,2> data;
+	for (unsigned i = 0; i < size; ++i) {
+		auto inter = ray.intersect(edge(i));
+		if (inter.hasHit()) data.addIntersection(inter);
+	}
+	
+	return data;
+}
+
+template <typename T>
+math::geometry::IntersectionData<T,2> SimplePolygon<T>::intersect(const math::geometry2::LineSegment<T>& line) const {
+	unsigned size = _vertices.size();
+	math::geometry::IntersectionData<T,2> data;
+	for (unsigned i = 0; i < size; ++i) {
+		auto inter = line.intersect(edge(i));
+		if (inter.hasHit()) data.addIntersection(inter);
+	}
+	
+	return data;
+}
+
+template <typename T>
+bool SimplePolygon<T>::isInside(const math::linear::StaticVector<T, 2>& point) const {
+	// Get some vectors.
+	math::linear::StaticVector<T, 2> center = this->center();
+	math::linear::StaticVector<T, 2> diff = point - center;
+	
+	// If point in question happens to be too near the center, just choose a random direction.
+	if (diff.dot() < 1e-4) diff = math::linear::StaticVector<T, 2>({0.6570, 0.5480});
+		
+	// Directed ray connecting point in question to the center of the polygon.
+	math::geometry2::Ray<T> ray(point, diff);
+	
+	// Run intersections.
+	auto inter = this->intersect(ray);
+	
+	// Decide if inside or outside.
+	if (inter.numberOfHits() % 2) return true;
+	else return false;
 }
 
 
--- test/gtest_src/math/geometry/2D/simple_polygon.cpp
+++ test/gtest_src/math/geometry/2D/simple_polygon.cpp
@@ -30,14 +30,65 @@ TEST(SimplePolygon2D, ConstructorAndBasicFunctions) {
 	trapezoid.addVertex(2.0f * i);
 
 	EXPECT_EQ(square.numberOfEdges(), 4);
+	EXPECT_EQ(square.numberOfVertices(), 4);
+	EXPECT_EQ(square.edge(0).displacement(), i);
+	EXPECT_EQ(square.edge(1).displacement(), j);
+	EXPECT_EQ(square.edge(2).displacement(), -i);
+	EXPECT_EQ(square.edge(3).displacement(), -j);
 	EXPECT_FLOAT_EQ(square.perimeter(), 4.0);
 	EXPECT_FLOAT_EQ(square.area(), 1.0);
 	
 	EXPECT_EQ(triangle.numberOfEdges(), 3);
+	EXPECT_EQ(triangle.numberOfVertices(), 3);
 	EXPECT_FLOAT_EQ(triangle.perimeter(), 2.0 + std::sqrt(2.0));
 	EXPECT_FLOAT_EQ(triangle.area(), 0.5);
 	
 	EXPECT_EQ(trapezoid.numberOfEdges(), 4);
+	EXPECT_EQ(trapezoid.numberOfVertices(), 4);
 	EXPECT_FLOAT_EQ(trapezoid.perimeter(), 4.0 + std::sqrt(2.0));
 	EXPECT_FLOAT_EQ(trapezoid.area(), 3.0 / 2.0);
+}
+
+TEST(SimplePolygon2D, IntersectionOperations) {
+	// Define few vectors.
+	math::linear::StaticVector<float, 2> zero({0.0, 0.0});
+	math::linear::StaticVector<float, 2> i({1.0, 0.0});
+	math::linear::StaticVector<float, 2> j({0.0, 1.0});
+	math::linear::StaticVector<float, 2> one = i + j;
+	
+	// Define few segments.
+	math::geometry2::SimplePolygon<float> square;
+	square.addVertex(zero);
+	square.addVertex(i);
+	square.addVertex(i+j);
+	square.addVertex(j);
+	
+	math::geometry2::SimplePolygon<float> triangle;
+	triangle.addVertex(zero);
+	triangle.addVertex(j);
+	triangle.addVertex(i);
+	
+	math::geometry2::SimplePolygon<float> trapezoid;
+	trapezoid.addVertex(zero);
+	trapezoid.addVertex(j);
+	trapezoid.addVertex(i+j);
+	trapezoid.addVertex(2.0f * i);
+	
+	math::geometry2::LineSegment<float> segment(0.5f * one - i, 0.5f * one);
+	EXPECT_FALSE(segment.intersect(square.edge(0)).hasHit());
+	EXPECT_FALSE(segment.intersect(square.edge(1)).hasHit());
+	EXPECT_FALSE(segment.intersect(square.edge(2)).hasHit());
+	
+	auto inter = segment.intersect(square.edge(3));
+	EXPECT_FLOAT_EQ(inter.thisParameter(), 0.5);
+	EXPECT_FLOAT_EQ(inter.otherParameter(), 0.5);
+	
+	inter = square.intersect(segment);
+	EXPECT_TRUE(inter.hasHit());
+	EXPECT_FLOAT_EQ(inter.thisParameter(), 0.5);
+	EXPECT_FLOAT_EQ(inter.otherParameter(), 0.5);
+	
+	EXPECT_FALSE(square.isInside(0.5f * one - i));
+	EXPECT_TRUE(square.isInside(0.5f * one - 0.2f * j));
+	EXPECT_TRUE(square.isInside(0.5f * one));
 }
