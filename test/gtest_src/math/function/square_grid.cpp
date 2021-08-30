5ef9fce4fe4bf01ed413973b59dce0cd84ee9da1
Physicist137 <physicist093@yahoo.com.br>
2021-08-30 16:48:28 -0300

    Improve test in Square Grid function, and add partial derivative test

--- test/gtest_src/math/function/square_grid.cpp
+++ test/gtest_src/math/function/square_grid.cpp
@@ -40,4 +40,34 @@ TEST(SquareGridTest, ConstructorAndBasics) {
 	EXPECT_FLOAT_EQ(grid.evaluate(1.0,0.0), 0.0);
 	EXPECT_FLOAT_EQ(grid.evaluate(1.0,1.0), 4.0);
 	EXPECT_FLOAT_EQ(grid.evaluate(0.5,0.5), 1.0);
+	
+	
+	unsigned size = 10;
+	float spacing = 1.0 / static_cast<float>(size);
+	SquareGridFunction<float, float> larger_grid(size, size, spacing, zero);
+	for (unsigned i = 0; i < size; ++i) {
+		for (unsigned j = 0; j < size; ++j) {
+			EXPECT_FLOAT_EQ(larger_grid.dataEvaluation(i,j), 0.0);
+		}
+	}
+	
+	larger_grid.setValueAllSquares(2.0);
+	for (unsigned i = 0; i < size; ++i) {
+		for (unsigned j = 0; j < size; ++j) {
+			EXPECT_FLOAT_EQ(larger_grid.dataEvaluation(i,j), 2.0);
+		}
+	}
+}
+
+TEST(SquareGridTest, GradientTests) {
+	math::linear::StaticVector<float, 2> zero({0.0, 0.0});
+	math::linear::StaticVector<float, 2> i({1.0, 0.0});
+	math::linear::StaticVector<float, 2> j({0.0, 1.0});
+	
+	SquareGridFunction<float, float> grid(10, 10, 0.1, {0.0, 0.0});
+	grid.setValueAllSquares(2.0);
+	auto partialx = grid.partial_x();
+	
+	EXPECT_FLOAT_EQ(grid.dataEvaluation(5, 5), 2.0);
+	EXPECT_FLOAT_EQ(partialx.evaluate(0.4, 0.4), 0.0);
 }
